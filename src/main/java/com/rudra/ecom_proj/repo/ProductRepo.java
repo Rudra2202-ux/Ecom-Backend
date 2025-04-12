e8c94c7855bc66214da8815159d4ccb3b1d4a425
Rudra Pratap Singh <rudrasingh22102@gmail.com>
2025-04-12 09:48:45 +0530

    first commit

---
+++ src/main/java/com/rudra/ecom_proj/repo/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.rudra.ecom_proj.repo;
+
+import com.rudra.ecom_proj.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
---
+++ src/test/java/com/rudra/ecom_proj/EcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra.ecom_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
